17bc1e84657834f596136e74ac530b6c585b26ad
Isaac Sanchez <isaacsanchez@MacBook-Pro-de-Isaac.local>
2017-07-18 20:49:36 +0200

    Creación proyecto Firebase

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
+import firebase from 'firebase'
 
 import App from './components/App'
+ 
+firebase.initializeApp({
+  apiKey: 'AIzaSyDJi4I5rXIENxD2RFyWF5W3LmYYgGwFTMg',
+  authDomain: 'curso-react-81033.firebaseapp.com',
+  databaseURL: 'https://curso-react-81033.firebaseio.com',
+  projectId: 'curso-react-81033',
+  storageBucket: '',
+  messagingSenderId: '994608383274'
+});
 
 render(<App />, document.getElementById('root'))
